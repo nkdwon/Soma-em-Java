3c22d2d4024e8df0b68fbd7846cc4bc897ae01fa
Felipe <felipebarrosratton.almeida@gmail.com>
2024-08-13 16:59:34 -0300

    Update Soma.java

--- soma/src/soma/Soma.java
+++ soma/src/soma/Soma.java
@@ -1,10 +1,26 @@
 package soma;
 
-public class Soma {
+import java.util.*;
 
+class Soma {
+	
+	public static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá Mundo!");
+		int num1, num2, soma;
+		
+		System.out.println("Soma de 2 números!!");
+		
+		System.out.println("Insira o primeiro número: ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Insira o segundo número: ");
+		num2 = sc.nextInt();
+		
+		soma = num1 + num2;
+		
+		System.out.println("Soma dos 2 números é de: " + soma);
 	}
 
 }
